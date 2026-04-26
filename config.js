5bfd96019aa6a05b6052b4436769c53110cd7896
eiji.hatakeyama <e.hatakeyama@april-knights.com>
2026-04-26 10:40:41 +0900

    fix: GITHUB_PAGES_ORIGIN URL修正

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // GAS エンドポイント設定
 const GAS_AUTH_URL = 'https://script.google.com/a/macros/april-knights.com/s/AKfycbyK8YnqmzFnWUjicK1v-pJ4bUcr38tHmLUWWDsx49Qfk0DJycYGQFxp4fKconcvojtZew/exec';
 const GAS_API_URL  = 'https://script.google.com/macros/s/AKfycbx3Y3YMynssVLnUy2awDXp-Qc9f0dWicwSWcgeZuXE_kovvdyjyzCoKxpqJXM_W4xJH/exec';
-const GITHUB_PAGES_ORIGIN = 'https://april-knights-dev.github.io/pj-ai-native-portal/';
+const GITHUB_PAGES_ORIGIN = 'https://april-knights-dev.github.io/pj-ai-native-portal-pages/';
